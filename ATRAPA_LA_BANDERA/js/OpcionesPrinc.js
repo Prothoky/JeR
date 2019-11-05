d8fbe55ad9919f5ee08d12cbdb25264b36149d43
pauula14 <paulacalzada014@gmail.com>
2019-11-05 18:19:00 +0100

    opcionesPrinc y enu princ phaser 3

--- ATRAPA_LA_BANDERA/js/OpcionesPrinc.js
+++ ATRAPA_LA_BANDERA/js/OpcionesPrinc.js
@@ -1,14 +1,17 @@
-Pantalla.OpcionesPrinc = function (game){};
+class OpcionesPrinc extends Phaser.Scene {
 
-Pantalla.OpcionesPrinc.prototype = {
+	constructor(){
 
-	create: function(){
+		super({key: "OpcionesPrinc"});
 
+	}
+
+	create(){
 		this.add.sprite (80, 40, 'fondoOpciones');
 		this.add.sprite(566, 77, 'opciones');
 		this.add.sprite (312, 212.5, 'cuadroVolumen');
 		this.add.sprite(710, 232.5, 'volumen');
-				
+
 		//BOTONES DE VOLUMEN
 		this.botonSubir = this.add.button(417.665, 307.5, 'subirVolumen', this.subir, this, 2, 0, 1);
 		this.botonSubir.input.useHandCursor = true;
@@ -23,25 +26,24 @@ Pantalla.OpcionesPrinc.prototype = {
 		this.botonVolver = this.add.button(612.5, 540, 'volverOpciones', this.volver, this, 2, 0, 1);
 		//this.botonVolver.anchor.set(-2.5,-3);
 		this.botonVolver.input.useHandCursor = true;
+	}
 
-	},
-
-	volver: function() {
+	volver() {
 
 		//una vez hecho esto, vamo a las instrucciones
 		this.game.state.start('MenuPrincipal');
-	}, 
+	}
 
-	subir: function(){
-
-	}, 
-
-	bajar: function(){
-
-	},
-
-	quitar: function(){
+	subir(){
 
 	}
 
-};
+	bajar(){
+
+	}
+
+	quitar:(){
+
+	}
+
+}
