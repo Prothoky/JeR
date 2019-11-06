560bee127f788b40c4dcc0183d1714a88d6554df
19eusebiu98 <deusebiu98@yahoo.es>
2019-11-07 00:54:16 +0100

    help

--- ATRAPA_LA_BANDERA/js/Juego.js
+++ ATRAPA_LA_BANDERA/js/Juego.js
@@ -67,7 +67,7 @@
 
 		create(){
 <<<<<<< HEAD
-		//Controles por teclado
+		//Controles por tecladoo
 
 		//Controles J1(rojo)
 		this.A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
