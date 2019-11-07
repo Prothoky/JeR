ffb35212899baae407ede4a456ad6a4de649461b
pauula14 <paulacalzada014@gmail.com>
2019-11-07 04:34:20 +0100

    Fin de nivel

--- ATRAPA_LA_BANDERA/js/FinNivel.js
+++ ATRAPA_LA_BANDERA/js/FinNivel.js
@@ -5,4 +5,52 @@ class FinNivel extends Phaser.Scene {
 		super ({key: "FinNivel"});
 
 	}
+
+	preload(){
+
+		//el fondo del fin de nivel
+		//this.load.image('fondoFin', 'assets/img/FinNivel/fondoFin.png');
+		//cargamos el logo arriba
+		//this.load.image('logo', 'assets/img/FinNivel/logo.png');g');
+		//boton de salir
+		//this.load.image('salir', 'assets/img/FinNivel/salir.png');
+
+		var texto;
+
+	}
+
+	create(){
+
+		var height = game.config.height;
+		var width = game.config.width;
+
+		var x = width/2 ;
+		var y = height/2;
+
+		var fondoFin = this.add.sprite (x, y, 'fondoFin');
+
+		fondoFin.displayWidth = width;
+		fondoFin.scaleX = fondoFin.scaleY;
+
+		texto = this.add.text(16, 16, 'Ganador: \n Perdedor:', { fontSize: '32px', fill: '#000' });
+
+/*
+		this.botonSalir = this.add.image(x, y*12/8, 'abandonar');
+		this.botonSalir.setInteractive({ useHandCursor: true  } )
+		.on('pointerdown', () => this.salir());
+*/
+
+		if(j1 == winner){
+			texto.setText('Ganador: JUGADOR 1\n Perdedor:JUGADOR 2');
+		}else{
+			texto.setText('Ganador: JUGADOR 2\n Perdedor:JUGADOR 1');
+		}
+	}
+
+	salir (){
+
+		this.scene.start('MenuPrincipal');
+
+	}
+
 }
